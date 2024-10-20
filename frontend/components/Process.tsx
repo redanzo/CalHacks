b19b8b4b1c26ffcb8b6245c6b2cfab02d8629b73
Kurasad <akowaidev@gmail.com>
2024-10-19 22:24:24 -0500

    loading

--- frontend/components/Process.tsx
+++ frontend/components/Process.tsx
@@ -1,6 +1,14 @@
 import styles from './jobs.module.css'
 
 export default function Jobs() {
+
+    for (let i = 0; i <= 1000; i++) {
+        setTimeout(() => {
+            document.documentElement.style.setProperty('--load', i / 10 === 100 ? '100%' : `${i / 10 < 10 ? `0${i / 10}` : i / 10}${i % 10 === 0 ? '.0' : ''}%`)
+            document.querySelector('#woot div')!.setAttribute('data-percent', i / 10 === 100 ? '100%' : `${i / 10 < 10 ? `0${i / 10}` : i / 10}${i % 10 === 0 ? '.0' : ''}%`)
+        }, i * 20)
+    }
+
     return (
         <>
             <div style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'fixed', zIndex: 200 }} className={styles.jobs}>
@@ -17,7 +25,7 @@ export default function Jobs() {
                     justifyContent: 'center',
                 }}>
 
-                    <div className={styles.load}>
+                    <div className={styles.load} id='woot'>
                         <div data-percent="0.0%"></div>
                     </div>
                 </main>
